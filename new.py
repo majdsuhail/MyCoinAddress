2dcdab990ebffa56b820e3a4f7c252e10f483fc2
majdsuhail <52085317+majdsuhail@users.noreply.github.com>
2022-11-20 09:57:09 +0300

    Create new.py

---
+++ new.py
@@ -0,0 +1,4 @@
+import urllib2
+
+for line in urllib2.urlopen(https://github.com/majdsuhail/ShebaEmlakTemplateTool-SETT-/blob/b4e08053bb78a98fdbe9a823f0bc554c918773b3/something.py):
+    print (line)

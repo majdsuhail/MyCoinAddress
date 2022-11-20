f53af6da67354988a86f67d8334e15f31656df48
majdsuhail <52085317+majdsuhail@users.noreply.github.com>
2022-11-20 09:59:19 +0300

    Delete new.py

--- new.py
+++
@@ -1,4 +0,0 @@
-import urllib2
-
-for line in urllib2.urlopen(https://github.com/majdsuhail/ShebaEmlakTemplateTool-SETT-/blob/b4e08053bb78a98fdbe9a823f0bc554c918773b3/something.py):
-    print (line)
